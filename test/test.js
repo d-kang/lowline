028484234a9a5825737c30e6b950e6461dd4babb
d-kang <david.kang714@gmail.com>
2017-11-06 08:53:39 -0800

    Refactor to Arrow Functions

--- test/test.js
+++ test/test.js
@@ -3,19 +3,18 @@ const _ = require('../lowline');
 
 console.log('============//////////////==============');
 console.log(new Date().toLocaleTimeString());
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
+describe('Array', () => {
+  describe('#indexOf()', () => {
+    it('should return -1 when the value is not present', () => {
       assert.equal(-1, [1, 2, 3].indexOf(4));
     })
   })
 })
 
-console.log('_', JSON.stringify(_))
-describe('_', function() {
-  describe('_', function() {
-    it('should import _ successfully', function() {
-      assert.equal(_, 'hello world')
+describe('_', () => {
+  describe('_', () => {
+    it('should import _ successfully', () => {
+      assert.equal(_, 'hello world');
     })
   })
 })
