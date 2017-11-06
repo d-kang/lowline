7bf36aea0ad0be6a5771302f069b8461b758a6a2
d-kang <david.kang714@gmail.com>
2017-11-06 09:45:24 -0800

    Add in ESLINT With ES6 Rules

--- lowline/index.js
+++ lowline/index.js
@@ -2,7 +2,7 @@ const _ = {};
 
 _.chunk = () => {
   
-}
+};
 
 
 module.exports = _;
--- test/test.js
+++ test/test.js
@@ -10,29 +10,29 @@ describe('Array', () => {
   describe('#indexOf()', () => {
     it('should return -1 when the value is not present', () => {
       assert.equal(-1, [1, 2, 3].indexOf(4));
-    })
-  })
-})
+    });
+  });
+});
 
 describe('_', () => {
   describe('_', () => {
     it('should import _ successfully', () => {
       expect(_).to.not.equal(undefined);
-    })
+    });
     it('_ should be an object', () => {
       expect(_).to.be.a('object');
-    })
-  })
-})
+    });
+  });
+});
 
 
 describe('_.chunk', () => {
   describe('_.chunk', () => {
     it('it should be a function', () => {
       expect(_.chunk).to.be.a('function');
-    })
+    });
     it('_ should be an object', () => {
       expect(typeof _).to.equal('object');
-    })
-  })
-})
+    });
+  });
+});
