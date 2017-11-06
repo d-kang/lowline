e2fd84be534b3ebd96820b56e3539f5a97126110
d-kang <david.kang714@gmail.com>
2017-11-06 08:52:23 -0800

    Require _ Successfully

--- test/test.js
+++ test/test.js
@@ -1,4 +1,5 @@
 const assert = require('assert');
+const _ = require('../lowline');
 
 console.log('============//////////////==============');
 console.log(new Date().toLocaleTimeString());
@@ -6,6 +7,15 @@ describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
       assert.equal(-1, [1, 2, 3].indexOf(4));
+    })
+  })
+})
+
+console.log('_', JSON.stringify(_))
+describe('_', function() {
+  describe('_', function() {
+    it('should import _ successfully', function() {
+      assert.equal(_, 'hello world')
     })
   })
 })
