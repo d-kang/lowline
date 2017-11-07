e5d64f64ca39a7791c6c2d3f73427f452a8330f8
d-kang <david.kang714@gmail.com>
2017-11-07 07:50:10 -0800

    Add in Test for ForEach

--- test/test.collection.js
+++ test/test.collection.js
@@ -1,12 +1,48 @@
 // describe('', () => {})
+
+// describe('', () => {
+//   it('', () => {
+//
+//   });
+//   it('', () => {
+//
+//   });
+// });
 (function() {
   'use strict';
+
+  const { expect } = chai;
   describe('Collection', () => {
-    describe('hi', () => {
+    describe('_.forEach', () => {
+      it('it should iterate over elem', () => {
+        const store = [];
+        _.forEach([1, 2, 3], (elem, i, arr) => {
+          store.push([elem, i]);
+        });
+        expect(store).to.deep.equal([[1,0], [2,1], [3,2]]);
+      });
+      it('it should work on objects', () => {
+        const store = [[1, 'a'], [2, 'b'], [3, 'c']];
+        _.forEach({a:1, b:2, c:3}, (val, key, obj) => {
+          return;
+          store.push([val, key]);
+        });
+        expect(store).to.deep.equal([]);
+      });
+
+    });
+
+    describe('', () => {
+      it('', () => {
+
+      });
       it('', () => {
 
       });
     });
-  });
 
+
+
+
+  });
 })();
